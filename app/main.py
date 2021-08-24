86d5d38bd12210346c86fff33ade31d42f0f6bd6
Mazwan98 <msulisstia@gmail.com>
2021-08-25 06:32:56 +0800

    Fix All

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
-from tkinter.constants import CENTER
 import tkinter.messagebox as tm
 import tkinter as tki
-# from typing_extensions import IntVar
-# from tki.constants import GROOVE
 import pyautogui as pag
 import time
-
+from datetime import date
 
 def main():
     app = tki.Tk()
-    app.title('Auto Screenshot  |   PYT-3C ( Kel. T )')
-    # app.geometry('650x250')
+    app.title('Auto Screenshot  |   ( Kel. T )')
+        
+    
+    def about():
+        tm.showinfo('Tentang Aplikasi', 'Tugas Project Kelas PYT - 3C DTS Kemenkominfo at Cisco (Kelompok T) :\n1. Sulistiawan A P\n2. Yusuf Bagaskoro\n3. Bernad Wayu')
+    
+    menubar = tki.Menu(app, tearoff=0, selectcolor='black')
+    menubar.add_command(label='Tentang', command=about)
+    
 
     var = tki.IntVar()
     var.set(1)
@@ -21,7 +25,7 @@ def main():
     var3 = tki.IntVar()
     var3.set(1)
 
-    def kosong():
+    def reset():
         var.set('')
         var2.set('')
         var3.set('')
@@ -31,19 +35,27 @@ def main():
         time.sleep(var.get())
 
         for i in range(var2.get()):
-            # tm.showinfo('Info Bro !', var.get())
-            filename = 'screenshot ' + str(i) + '.png'
+            
+            today = date.today()
 
-            # tampil = tki.Label(app, text='File ' + filename +' sedang dibuat . . .')
-            # tm.showinfo('Info Bro !', 'tes')
-            info = tki.Label(app, text='File ' + filename +
-                             ' sedang dibuat . . .', bg='black', fg='yellow')
-            info.grid(row=3, column=0, padx=3, pady=3)
+            filename = 'screenshot ' + str(today) + ' ' + str(i) + '.png'
+            folder = r'D:\Project Python\pyautoss\app\hasil' + '\\' + filename
+            
+            try:
+                info = tki.Label(app, text='File ' + filename +
+                                ' dibuat . . .', bg='black', fg='green')
+                info.grid(row=3, column=0, padx=3, pady=3)
+                
+                shot = pag.screenshot()
+                shot.save(folder)
 
-            shot = pag.screenshot()
-            shot.save(filename)
+                time.sleep(var3.get())
+                
+                
+            except:
+                info = tki.Label(app, text='Ups .. Ada yang salah ( Error ) saat menyimpan ' + filename , bg='black', fg='green')
+                info.grid(row=3, column=0, padx=3, pady=3)
 
-            time.sleep(var3.get())
 
     l = tki.Label(app, text='Durasi antar SS (Detik) ?')
     l.grid(row=0, column=0, sticky=tki.E, padx=3, pady=3)
@@ -63,35 +75,15 @@ def main():
     e3 = tki.Entry(app, textvariable=var3)
     e3.grid(row=2, column=1, columnspan=2, padx=3, pady=3)
 
-    b1 = tki.Button(app, text='Reset', command=kosong, activebackground='red')
+    b1 = tki.Button(app, text='Reset', command=reset, activebackground='red')
     b1.grid(row=3, column=1, padx=3, pady=3, sticky=tki.N+tki.S+tki.E+tki.W)
 
     b2 = tki.Button(app, text='Mulai', command=mulai,
                     activebackground='black', activeforeground='white')
     b2.grid(row=3, column=2, padx=3, pady=3, sticky=tki.N+tki.S+tki.E+tki.W)
 
-    # def mulai():
-    #     # tm.showinfo('Info', 'Halo Mas Bro !')
-
-    #     time.sleep(int(input('Jeda antar screenshot (Detik) ?\t')))
-
-    #     for i in range(int(input('Berapa Kali Screenshot ?\t'))):
-    #         filename = 'screenshot ' + str(i) + '.png'
-
-    #         tampil = print('File ' + filename +' sedang dibuat . . .')
-
-    #         tm.showinfo('Info Mas Bro !', tampil)
-
-    #         shot = pag.screenshot()
-    #         shot.save(filename)
-
-    #         time.sleep(1)
-
-    # btn = tki.Button(app, text='Start', state=tki.NORMAL, command=mulai)
-    # btn.grid(sticky=tki.E+tki.S, padx=90, pady=120)
-
+    app.config(menu=menubar)
     app.mainloop()
-
 
 if __name__ == "__main__":
     main()
